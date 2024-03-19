fd7f113cd92d77d954fdae9c696e7f06eb7e6a3c
Braden MacDonald <mail@bradenm.com>
2024-03-18 19:43:36 -0700

    feat: add mathjs comparisons to benchmarks

--- tests/benchmark.bench.ts
+++ tests/benchmark.bench.ts
@@ -2,6 +2,7 @@ import { Q as ourQ } from "../mod.ts";
 
 import JSQ_Qty from "npm:js-quantities@1.8.0";
 import PQM from "npm:pqm@1.0.0";
+import * as mathjs from "npm:mathjs@12.4.1";
 
 Deno.bench("Quantity conversions - quantity-math-js", { group: "conversion", baseline: true }, () => {
     const Q = ourQ;
@@ -65,4 +66,63 @@ Deno.bench("Quantity conversions - PQM", { group: "conversion" }, () => {
     if (converted.toString() !== "1.920207699666667e-8") {
         throw new Error(`Got ${converted.toString()} unexpectedly.`);
     }
+});
+
+Deno.bench("Quantity conversions - mathjs", { group: "conversion" }, () => {
+    const Q = mathjs.unit;
+    const a = Q(400, `g`);
+    const b = Q("0.5"); // Can't find a way to do "50 %" in mathjs ?
+    const c = a.multiply(b);
+    const d = c.multiply(Q(50, `m/s`)); // 10,000 g * m / s (= 10 kg * m / s)
+    const e = d.multiply(Q(0.1, `s^-1`)); // 1 kg * m / s^2 (= 1 N)
+    const f = mathjs.add(e, Q(5.5, `N`));
+    const g = mathjs.add(mathjs.add(f.multiply(Q("10")), Q(5, `N`)), Q(-20, `N`)).multiply(Q("2"));
+    const h = g.toSI();
+    // This library won't simplify to Newtons, since it's technically a derived unit.
+    if (h.toString() !== "100 (kg m) / s^2") throw new Error(`Got ${h.toString()} unexpectedly.`);
+
+    // And some crazy conversion:
+    const orig = Q(500, `uF`);
+    const converted = orig.to("hr s^3 A^2/(lbm m ft)");
+    if (converted.toString() !== "1.9202076996666667e-8 (hr s^3 A^2) / (lbm m ft)") {
+        throw new Error(`Got ${converted.toString()} unexpectedly.`);
+    }
+});
+
+Deno.bench("Custom units - quantity-math-js", { group: "custom", baseline: true }, () => {
+    const Q = ourQ;
+    const a = Q`400 _fleeb`;
+    const b = Q`50 %`;
+    const c = a.multiply(b); // 200 _fleeb
+    const d = c.multiply(Q`50 _bil/_boop`); // 10,000 _fleeb _bil / _boop
+    const e = d.multiply(Q`0.1 s^-1`); // 1,000 _fleeb _bil / _boop s
+    const f = e.add(Q`500 _fleeb _bil / _boop s`); // 1,500 _fleeb _bil / _boop s
+    const g = f.multiply(Q`10`).add(Q`5 _fleeb _bil / _boop s`).add(Q`-20 _fleeb _bil / _boop s`).multiply(Q`2`);
+    if (g.toString() !== "29970 _fleeb⋅_bil/_boop⋅s") throw new Error(`Got ${g.toString()} unexpectedly.`);
+
+    const h = g.multiply(Q`0.1 _schleem`).sub(Q`997 _fleeb _schleem _bil / _boop s`).multiply(Q`1 _boop / _bil`);
+    if (h.toString() !== "2000 _fleeb⋅_schleem/s") throw new Error(`Got ${h.toString()} unexpectedly.`);
+});
+
+mathjs.createUnit("fleeb");
+mathjs.createUnit("bil");
+mathjs.createUnit("boop");
+mathjs.createUnit("schleem");
+
+Deno.bench("Custom units - math-js", { group: "custom" }, () => {
+    const Q = mathjs.unit;
+    const a = Q(`400 fleeb`);
+    const b = Q("0.5"); // Can't find a way to do "50 %" in mathjs ?
+    const c = a.multiply(b); // 200 fleeb
+    const d = c.multiply(Q(`50 bil/boop`)); // 10,000 fleeb bil / boop
+    const e = d.multiply(Q(`0.1 s^-1`)); // 1,000 fleeb bil / boop s
+    const f = mathjs.add(e, Q(`500 fleeb bil / (boop s)`)); // 1,500 fleeb bil / boop s
+    const g = mathjs.add(mathjs.add(f.multiply(Q(`10`)), Q(`5 fleeb bil / (boop s)`)), Q(`-20 fleeb bil / (boop s)`))
+        .multiply(Q(`2`));
+    if (g.toString() !== "29970 (fleeb bil) / (boop s)") throw new Error(`Got ${g.toString()} unexpectedly.`);
+
+    const h = mathjs.subtract(g.multiply(Q(`0.1 schleem`)), Q(`997 fleeb schleem bil / (boop s)`)).multiply(
+        Q(`1 boop / bil`),
+    );
+    if (h.toString() !== "2000 (fleeb schleem) / s") throw new Error(`Got ${h.toString()} unexpectedly.`);
 });
